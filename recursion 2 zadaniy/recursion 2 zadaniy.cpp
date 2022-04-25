1e592c97666baa1e6d0e916e0b33391f0e07f164
Андрей Боярский <an0982978490@outlook.com>
2022-04-25 11:51:17 +0300

    Добавьте файлы проекта.

---
+++ recursion 2 zadaniy/recursion 2 zadaniy.cpp
@@ -0,0 +1,32 @@
+﻿//2. От A до B.
+//Даны два целых числа A и В.Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+	//Ввод: 5 1
+	//Вывод : 5 4 3 2 1
+#include <iostream>
+using namespace std;
+int n;
+void otrezok(int A, int B) {
+	if (A < B) {
+		cout << A << "\t";
+		A++;
+		 otrezok(A, B);
+
+		
+	}
+	if (A > B) {
+		cout << A << "\t";
+		A--;
+		otrezok(A, B);
+	}
+	else if (A == B) return;
+	
+		
+}
+int main()
+{
+	
+	otrezok(2, 5);
+	
+	
+}
+
